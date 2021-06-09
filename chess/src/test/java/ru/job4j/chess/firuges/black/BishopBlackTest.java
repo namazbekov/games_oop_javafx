ffdf696638e1cdbe899ead13c7b41e9f21b15fa8
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-06-09 21:08:54 +0300

    2. Каркас шахматной доски [#792]

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -16,6 +16,13 @@ public class BishopBlackTest {
     @Test
     public void testCopy() {
         BishopBlack bishopBlack = new BishopBlack(Cell.A2);
-        assertThat(bishopBlack.copy(Cell.A2), is(Cell.A2));
+        assertThat(bishopBlack.copy(Cell.A2).position(), is(Cell.A2));
+    }
+
+    @Test
+    public void testWay() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Cell[] rsl = new Cell[]{Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        assertThat(bishopBlack.way(Cell.G5), is(rsl));
     }
 }
